d67651ecb77877150f012d3c840b6a6e2c6ae3fe
Ken Rimple <kenrimple@honeycomb.io>
2025-07-14 16:16:28 -0400

    added better UI feedback

--- services/react/src/pages/Meminator.tsx
+++ services/react/src/pages/Meminator.tsx
@@ -1,28 +1,50 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Meminator() {
 
   const [image, setImage] = useState<Blob | null>(null);
+  const [phrase, setPhrase] = useState<string>('');
+  const [fetching, setFetching] = useState<boolean>(false);
   
-  useEffect(() => {
-    fetchMeme();
-  }, []);
-
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     fetchMeme();
   };
 
-  const fetchMeme = () => {
-    fetch('/api/createPicture', { method: 'POST' })
-        .then((response) => response.blob())
-        .then((data) => setImage(data));
+  const fetchMeme = async () => {
+    setFetching(true);
+    try {
+        const response = await fetch('/api/createPicture', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: phrase ? JSON.stringify({phrase: phrase}) : null
+        });
+        setImage(await response.blob());
+    } catch (e) {
+        alert("that was bad...");
+    } finally {
+        setFetching(false);
+    }
   };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      event.preventDefault();
+      const phraseText = event.target.value;
+      if (event.target.value) {
+          setPhrase(phraseText)
+      } else {
+          setPhrase('')
+      }
+  }
 
   return (
     <div>
+      <h3>Enter a phrase, then click 'GO'!</h3>
       <div className="block">
-         <button onClick={handleClick}>GO</button>
+      <input id="meme" onChange={handleChange} />
+         <button id="self-meme" disabled={fetching} onClick={handleClick}>GO</button>
       </div>
       <div>&nbsp;</div>
       <div className="block">
