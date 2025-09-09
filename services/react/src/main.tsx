5d19302c3427f031c470b980d179256fc6f613bf
dlsdlsdls <dianelestrain@gmail.com>
2025-09-09 16:32:05 -0700

    added a comma back in on line 19, accidentally removed this on first commit

--- services/react/src/main.tsx
+++ services/react/src/main.tsx
@@ -12,7 +12,7 @@ const sdk = new HoneycombWebSDK({
     serviceName: 'react',
     instrumentations: [
         getWebAutoInstrumentations()
-    ]
+    ],
     // Then, configure the `sessionProvider` property by implementing the `getSessionId` method. 
     // This calls our `getSession` function to return a valid `session.id`. 
     // This method will store this `session.id` in the browser's `sessionStorage` and reuse it across multiple "GO" clicks, page loads, and route changes, or generate a new one when needed.
