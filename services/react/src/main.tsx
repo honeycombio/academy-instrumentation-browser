3f6549ef640db73352012e10b003e7a37171226c
dlsdlsdls <dianelestrain@gmail.com>
2025-09-26 10:07:46 -0700

    revised solution branch for automatic instrumentation

--- services/react/src/main.tsx
+++ services/react/src/main.tsx
@@ -1,10 +1,23 @@
+// install npm packages in services/react before adding any instrumentation code here
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web'; // import the HoneycombWebSDK class
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web'; //import getWebAutoInstrumentations function
+// configure the HoneycombWebSDK
+const sdk = new HoneycombWebSDK({
+    apiKey: 'your ingest api key', // your API key goes here
+    serviceName: 'react',
+    instrumentations: [
+        getWebAutoInstrumentations()
+    ]
+});
+sdk.start();
+// React createRoot goes here
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+// once SDK is configured, rerun the app to see traces in Honeycomb
