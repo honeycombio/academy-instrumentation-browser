d17a3bf599a90a0fc360eb1302acac7facb5d5d4
dlsdlsdls <dianelestrain@gmail.com>
2025-09-26 11:21:30 -0700

    solution branch for custom resource attributes

--- services/react/src/features.ts
+++ services/react/src/features.ts
@@ -1,2 +1,2 @@
 // add to telemetry as app.features.ALLOW_QUESTIONS=<boolean>
-export const features = { ALLOW_USER_QUESTIONS: false };
+export const features = { ALLOW_USER_QUESTIONS: false }; // set to true to allow user questions
--- services/react/src/main.tsx
+++ services/react/src/main.tsx
@@ -1,18 +1,23 @@
-// install npm packages in services/react before adding any instrumentation code here
+// add automatic instrumentation for web first
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web'; // import the HoneycombWebSDK class
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web'; //import getWebAutoInstrumentations function
-// configure the HoneycombWebSDK
+import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { features } from './features'; // import the features object
+
 const sdk = new HoneycombWebSDK({
     apiKey: 'your ingest api key', // your API key goes here
     serviceName: 'react',
     instrumentations: [
         getWebAutoInstrumentations()
-    ]
-});
+    ],
+    resourceAttributes: { // Data in this object is applied to every trace emitted.
+    "feature_flag.key": "ALLOW_USER_QUESTIONS", // Specific to your app.
+    "feature_flag.result.variant": features.ALLOW_USER_QUESTIONS, // Specific to your app.// specify extra attributes through the resourceAttributes configuration option
+    },
+  });
 sdk.start();
 // React createRoot goes here
 createRoot(document.getElementById('root')!).render(
@@ -20,4 +25,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-// once SDK is configured, rerun the app to see traces in Honeycomb
+
